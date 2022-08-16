e3af1cb532987399e1ca68e104ff6989218e2357
Will B <will.brackenbury1@gmail.com>
2022-08-16 10:22:47 -0500

    Updates

--- src/constants.js
+++ src/constants.js
@@ -5,13 +5,22 @@ export const ABOUT =
 export const INTERESTS = "If you want to start a conversation, try these: ";
 
 export const NEWS = [
+  "Our paper on summarizing sets of related recommendations in cloud storage has been accepted to UIST 2022. Looking forward to traveling to Bend, Oregon!",
   "I successfully defended my PhD! Many thanks to my advisers and my committee for their support on this milestone",
   "My dissertation defense is scheduled for April 27th--an exciting end to a long journey",
   "Our paper on instructions for annotator rationales was accepted to NAACL 2022! Congratulations to Jamar Sullivan on his first-ever first-author paper",
-  "I presented our paper on KondoCloud, a tool for enhanced file management recommendations, at UIST 2021. Thank you to the conference organizers and attendees for fostering wonderful discussion",
 ];
 
 export const PUBLICATIONS = [
+  {
+    link: "../public/assets/summarize_final.pdf",
+    title:
+    "Summarizing Sets of Related ML-Driven Recommendations for Improving File Management in Cloud Storage",
+    authors:
+    "Will Brackenbury, Kyle Chard, Aaron J. Elmore, Blase Ur",
+    journal: "UIST 2022",
+  },
+
   {
     title:
       "Explaining Why: How Instructions and User Interfaces Impact Annotator Rationales When Labeling Text Data",
@@ -21,7 +30,7 @@ export const PUBLICATIONS = [
   },
 
   {
-    link: "../assets/kondocloud_final.pdf",
+    link: "../public/assets/kondocloud_final.pdf",
     title:
       "KondoCloud: Improving Information Management in Cloud Storage via Recommendations Based on File Similarity",
     authors:
@@ -30,7 +39,7 @@ export const PUBLICATIONS = [
   },
 
   {
-    link: "../assets/flock_final.pdf",
+    link: "../public/assets/flock_final.pdf",
     title:
       "Files of a Feather Flock Together? Measuring and Modeling How Users Perceive File Similarity in Cloud Storage",
     authors:
@@ -39,28 +48,28 @@ export const PUBLICATIONS = [
   },
 
   {
-    link: "../assets/chi19-ifttt-cameraready.pdf",
+    link: "../public/assets/chi19-ifttt-cameraready.pdf",
     title: "How Users Interpret Bugs in Trigger-Action Programming",
     authors:
       "Will Brackenbury, Abhimanyu Deora, Jillian Ritchey, Jason Vallee, Weijia He, Guan Wang, Michael L. Littman, Blase Ur",
     journal: "CHI 2019",
   },
   {
-    link: "../assets/hilda18.pdf",
+    link: "../public/assets/hilda18.pdf",
     title: "Draining the Data Swamp: A Similarity-based Approach",
     authors:
       "Will Brackenbury, Rui Liu, Mainack Mondal, Aaron J. Elmore, Blase Ur, Kyle Chard, Michael J. Franklin",
     journal: "HILDA 2018",
   },
   {
-    link: "../assets/p2038-shang.pdf",
+    link: "../public/assets/p2038-shang.pdf",
     title: "CYADB: A Database that Covers Your Ask",
     authors:
       "Zechao Shang, Will Brackenbury, Aaron J. Elmore, Michael J. Franklin",
     journal: "VLDB (Demo) 2018",
   },
   {
-    link: "../assets/p2098-kesslerfaulkner.pdf",
+    link: "../public/assets/p2098-kesslerfaulkner.pdf",
     title: "k-Regret Queries with Nonlinear Utilities",
     authors:
       "Taylor Kessler Faulkner*, Will Brackenbury*, Ashwin Lall (*co-first authors)",
